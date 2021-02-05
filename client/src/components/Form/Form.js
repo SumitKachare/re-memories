1cecfd333c6ae048557746a9a4fa5f37112bbeac
SUMITXD\sumit <sumitkachare12@gmail.com>
2021-02-06 02:27:45 +0530

    Initial commit

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,88 @@
+import React , {useState , useEffect} from 'react'
+import useStyles from './styles'
+import FileBase from 'react-file-base64'
+import {useDispatch} from 'react-redux'
+import {TextField , Button , Typography , Paper} from '@material-ui/core'
+import { createPost , updatePost} from '../../actions/posts'
+import {useSelector} from 'react-redux'
+
+    
+
+const Form = ({currentId , setCurrentId}) => {
+    const classes = useStyles() 
+
+    const dispatch = useDispatch()
+
+    const user = JSON.parse(localStorage.getItem('profile'))
+    const [postData , setPostData] = useState({
+        title : '',
+        message : '',
+        tags : '',
+        selectedFile : ''
+    })
+
+    const post = useSelector((state)=>(currentId ? state.posts.find((post)=>post._id === currentId) : null))
+
+    useEffect(() => {
+        if (post) {
+            setPostData(post)
+        }
+    }, [post])
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        
+        if (currentId===0) {
+            dispatch(updatePost( currentId,{...postData, name : user?.result?.name})) 
+            clear()
+        } else { 
+            dispatch(createPost({...postData, name : user?.result?.name}))
+            clear()
+        } 
+
+    }
+
+    const clear = ()=>{
+        setCurrentId(0)
+        setPostData({
+        title : '',
+        message : '',
+        tags : '',
+        selectedFile : ''
+        })
+    }
+
+    if (!user?.result?.name) {
+        return(
+            <Paper className={classes.paper}>
+                <Typography variant="h6" align="center">
+                    Sign in to create your Memories!
+                </Typography>
+            </Paper>
+        )
+    }
+
+    return (
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6"> {currentId ? 'Editing' : 'Creating'} a Memory</Typography>
+
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) =>setPostData({...postData , title : e.target.value})} />
+
+                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) =>setPostData({...postData , message : e.target.value})} />
+
+                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) =>setPostData({...postData , tags : e.target.value.split(',')})} />
+
+                <div className={classes.fileInput}>
+                    <FileBase type="file" multiple={false} onDone={({base64})=> setPostData({...postData , selectedFile : base64})} />
+                </div>
+
+                <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth >Submit</Button>
+
+                <Button  variant="contained" color="secondary" size="small" onClick={clear} fullWidth >Clear</Button>
+            </form>
+        </Paper>
+    )
+}
+
+export default Form
---
+++ server/controllers/posts.js
@@ -0,0 +1,65 @@
+import PostMessage from '../models/postMessage.js'
+import mongoose from 'mongoose';
+
+
+export const getPosts = async(req , res)=>{
+    try {
+        const postMessages =  await PostMessage.find()
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+} 
+
+export const createPost = async(req ,res)=>{
+    const post =  req.body
+    const newPost =  new PostMessage({...post , creator : req.userId , createdAt : new Date().toISOString()})
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json(error)
+    }
+}
+
+export const updatePost =  async(req, res) =>{
+    const {id : _id} = req.params
+    const post = req.body
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post to update with that id')
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id , {...post , _id} , {new : true})
+    res.json(updatedPost)
+} 
+
+export const deletePost = async (req , res) =>{
+    const {id : _id} = req.params
+    const post =  req.body
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post  to delete with that id')
+    }
+    const deletePost = await PostMessage.findByIdAndRemove(_id)
+    res.json({message : "Post deleted"})
+}
+
+export const likePost = async (req , res)=>{
+    const {id} = req.params
+    if (!req.userId) {
+        return res.json({message : 'User not Authenticated'})
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No post to like with that id')
+    }
+    const post  = await PostMessage.findById(id)
+
+    const index = post.likes.findIndex((id)=>id===String(req.userId))
+
+    if (index===-1) {
+        post.likes.push(req.userId)
+    }else{
+        post.likes.filter((id)=>id !== String(req.userId))
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id , post , {new : true})
+    res.json(updatedPost)
+}
